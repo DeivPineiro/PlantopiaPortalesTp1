1bdc51882aa536fc863521e5c33dfd850616ca96
David Ezequiel Piñeiro <101911710+DeivPineiro@users.noreply.github.com>
2023-10-06 21:21:34 -0300

    Carga de noticia con validacion de datos.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,16 +13,15 @@ class AdminController extends Controller
     public function index()
     {
 
-       $news = Noticia::all();   
+        $news = Noticia::all();
 
-       return view('admin.index', ['news' => $news]);     
-
+        return view('admin.index', ['news' => $news]);
     }
 
     public function details(int $id)
     {
-        
-        return view('admin.details', ['new' => Noticia::findOrFail($id)]);// findOrFail -> Si no lo encuentra 404
+
+        return view('admin.details', ['new' => Noticia::findOrFail($id)]); // findOrFail -> Si no lo encuentra 404
 
     }
 
@@ -30,18 +29,36 @@ class AdminController extends Controller
     {
 
         return view('admin.create');
-
     }
 
     public function creating(Request $request)
     {
+        
+        $data = $request->except(['_token']);
+        
+        $request->validate([
 
-        dd($request);
+            'titulo' => 'required|min:2',
+            'subTitulo' => 'required|min:1',
+            'parrafo' => 'required|min:1',
+            'fecha_creacion' => 'required',
+            'editor' => 'required',
+            'publicado' => 'required'
 
-        //return view('admin.creating');
+        ],[
+            'titulo.required' => 'El titulo es necesario.',
+            'titulo.min' => 'El titulo tiene un minimo de :min caracteres',
+            'subTitulo.required' => 'El sub titulo es necesario.',
+            'subTitulo.min' => 'El sub titulo tiene un minimo de :min caracteres',
+            'parrafo.required' => 'El parrafo es necesario.',
+            'parrafo.min' => 'El parrafo tiene un minimo de :min caracteres',
+            'fecha_creacion.required' => 'La fecha de creación es necesario.',
+            'editor.required' => 'El editor es necesario.',
+            'publicado.required' => 'Es necesario saber si esta publicado al momento de crearlo.'
+        ]);
+
+        Noticia::create($data);
+        return redirect('/admin/noticias');
 
     }
-
-
-
 }
--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -4,32 +4,74 @@
 
 @section('content')
 
-<link rel="stylesheet" href="<?= url('css/bootstrap.min.css') ?>">
-<div class="container p-4">
-    <form action="{{url ('/admin/noticias/crear')}}" method="POST">
+    <link rel="stylesheet" href="<?= url('css/bootstrap.min.css') ?>">
+
+    <h1>Crear nueva noticia.</h1>
+
+    @if ($errors->any())
         
-@csrf {{-- Token de seguridad para evitar peticiones de otro sitio --}}
+            <p class="text-danger m-4">Error en la carga, revise datos ingresados.</p>
+        
+    @endif
 
-        <label for="titulo" class="form-label">Titulo</label>
-        <input type="text" id="titulo" name="titulo" class="form-control">
+    <div class="container p-4">
 
-        <label for="subtitulo" class="form-label">Sub titulo</label>
-        <input type="text" id="subtitulo" name="subtitulo" class="form-control">
+        <form action="{{ url('/admin/noticias/crear') }}" method="POST">
 
-        <label for="parrafo" class="form-label">Parrafo</label>
-        <textarea name="parrafo" id="parrafo" class="form-control"></textarea>
+            @csrf {{-- Token de seguridad para evitar peticiones de otro sitio --}}
 
-        <label for="imagen" class="form-label">Imagen</label>
-        <input type="file" id="imagen" name="imagen" class="form-control">
+            <label for="titulo" class="form-label">Titulo</label>
+            <input type="text" id="titulo" name="titulo" class="form-control">
 
-        <label for="editor" class="form-label">Editor</label>
-        <input type="text" id="editor" name="editor" class="form-control">
+            @if($errors->has('titulo'))
+            <p class="text-danger">{{$errors->first('titulo')}}</p>
+            @endif
 
-        <label for="fecha" class="form-label">fecha creación</label>
-        <input type="date" id="fecha" name="fecha" class="form-control">
+            <label for="subTitulo" class="form-label">Sub titulo</label>
+            <input type="text" id="subTitulo" name="subTitulo" class="form-control">
 
-        <button type="submit" class="btn btn-primary">Crear noticia</button>
+            @if($errors->has('subTitulo'))
+            <p class="text-danger">{{$errors->first('subTitulo')}}</p>
+            @endif
 
-    </form>
-</div>
+            <label for="parrafo" class="form-label">Parrafo</label>
+            <textarea name="parrafo" id="parrafo" class="form-control"></textarea>
+
+            @if($errors->has('parrafo'))
+            <p class="text-danger">{{$errors->first('parrafo')}}</p>
+            @endif
+
+            <label for="imagen" class="form-label">Imagen</label>
+            <input type="file" id="imagen" name="imagen" class="form-control">
+
+            @if($errors->has('imagen'))
+            <p class="text-danger">{{$errors->first('imagen')}}</p>
+            @endif
+
+            <label for="editor" class="form-label">Editor</label>
+            <input type="text" id="editor" name="editor" class="form-control">
+
+            @if($errors->has('editor'))
+            <p class="text-danger">{{$errors->first('editor')}}</p>
+            @endif
+
+            <label for="fecha_creacion" class="form-label">fecha creación</label>
+            <input type="text" id="fecha_creacion" name="fecha_creacion" class="form-control">
+
+            @if($errors->has('fecha_creacion'))
+            <p class="text-danger">{{$errors->first('fecha_creacion')}}</p>
+            @endif
+
+            <label for="publicado" class="form-label">Publicado</label>
+            <input type="text" id="publicado" name="publicado" class="form-control">
+
+            @if($errors->has('publicado'))
+            <p class="text-danger">{{$errors->first('publicado')}}</p>
+            @endif
+
+            <button type="submit" class="btn btn-primary my-4">Crear noticia</button>
+
+        </form>
+
+    </div>
 @endsection
